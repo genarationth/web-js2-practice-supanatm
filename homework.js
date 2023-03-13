5e95dfcd7648685c2144e31af19730670c466da8
supanatm <spnstation8@gmail.com>
2023-03-13 09:31:31 +0700

    homework2

---
+++ homework.js
@@ -0,0 +1,14 @@
+//challenge 1
+let bornYear = 2000;
+let futureYear = 2050;
+
+console.log(`If you were born in ${bornYear}, then in ${2026} you'll be ${futureYear - bornYear}`);
+
+//challenge 2
+let inFormation = 
+{
+    name: "Supanat",
+    location: "Nonthaburi",
+    hobby: "Football",
+}
+console.log(`Hi, my name is ${inFormation.name}. I live in ${inFormation.location} and my hobby is playing ${inFormation.hobby}.`);
